62c9f863a46322d44b7175a8ab2b571bfbfe974f
Alexander <AlexanderBely98@gmail.com>
2019-07-14 03:38:36 +0300

    Temporary solution of bug fix, when user could creates a duplicate sessions of him.

--- WebServerApplication/src/main/java/TouchSoftLabs/WebSocket/WebServerEndpoint.java
+++ WebServerApplication/src/main/java/TouchSoftLabs/WebSocket/WebServerEndpoint.java
@@ -20,6 +20,7 @@ import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 @ServerEndpoint(value = "/{userrole}/{username}", decoders = MessageDecoder.class, encoders = MessageEncoder.class)
@@ -31,14 +32,28 @@ public class WebServerEndpoint {
     private static LinkedList<String> usersQueue = new LinkedList<>();
     @Getter
     private static HashMap<Integer, ChatRoom> chatRooms = new HashMap<>();
+    @Getter
+    private static HashSet<String> userNames = new HashSet<>();
+
+    private String username;
 
     @OnOpen
     public void onOpen(Session session, @PathParam("username") String username, @PathParam("userrole") String userrole) throws IOException, EncodeException {
-        Client client = ServerService.registerClient(username, userrole, session);
-        ServerService.sendMessageFromServerToUser("Ваш идентификатор сессии: " + session.getId(), client);
-        ServerService.sendMessageFromServerToUser(String.format("Вы подключились к чату под логином \"%s\"", username), client);
-        ServerLogger.logInfo(String.format("Пользователь \"%s\" подключился к чату", username));
-        ServerService.checkStoredMessagesAndConnectIfAgent(client);
+        if(userNames.contains(username)){
+            Client client = new Client();
+            client.setSession(session);
+
+            ServerService.sendMessageFromServerToUser("Вы уже подключены к чату. Отключитесь от него, чтобы создать новую сессию.", client);
+            session.close();
+        } else {
+            this.username = username;
+            userNames.add(username);
+            Client client = ServerService.registerClient(username, userrole, session);
+            ServerService.sendMessageFromServerToUser("Ваш идентификатор сессии: " + session.getId(), client);
+            ServerService.sendMessageFromServerToUser(String.format("Вы подключились к чату под логином \"%s\"", username), client);
+            ServerLogger.logInfo(String.format("Пользователь \"%s\" подключился к чату", username));
+            ServerService.checkStoredMessagesAndConnectIfAgent(client);
+        }
     }
 
     @OnMessage
@@ -48,6 +63,7 @@ public class WebServerEndpoint {
 
     @OnClose
     synchronized public void onClose(Session session) throws IOException, EncodeException {
+        userNames.remove(username);
         ServerService.closeConnection(session);
     }
 
